7f31ded77d0cdb8d6d52e03f908ec39478660543
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-07 16:47:48 +0000

    Episode 4 complet

---
+++ who_dunnit_ep4.js
@@ -0,0 +1,18 @@
+//Episode 4
+
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
+
+// Predictions:
+// suspects = Miss Scarlet, Professor Plum, Colonel Mustard
+// Suspect three is Mrs. Peacock.
