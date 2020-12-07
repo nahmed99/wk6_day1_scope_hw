d8e928957ea82812d368cc4b034b5ee3d9fa6f1f
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-07 16:56:06 +0000

    Episode 7 complet

---
+++ who_dunnit_ep7.js
@@ -0,0 +1,30 @@
+// Episode 7
+
+let murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    let murderer = 'Colonel Mustard';
+
+    const unexpectedOutcome = function() {
+      murderer = 'Miss Scarlet';
+    }
+
+    unexpectedOutcome();
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// Prediction:
+// The murderer is Mr. Green - because plotTwist and unexpectedOutcome will use the local variable called murdered - as that is more 'in-scope' to them...
