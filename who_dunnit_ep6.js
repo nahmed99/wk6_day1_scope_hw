4d4a7ea7f652639cdbd199c7bb5fa118a22168a5
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-07 16:52:32 +0000

    Episode 6 complet

---
+++ who_dunnit_ep6.js
@@ -0,0 +1,24 @@
+// Episode 6
+
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// Prediction:
+// The murderer is Mrs. White.
