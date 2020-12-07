141de3f92724d93ae58521fb60afa46c1e9f6a03
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-07 16:42:01 +0000

    Episode 2 complet

---
+++ who_dunnit_ep2.js
@@ -0,0 +1,17 @@
+//Episode 2
+
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+//Prediction: changeMurderer should be allowed to change the value of (const) murderer
