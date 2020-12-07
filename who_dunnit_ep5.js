fc98266afec7f0f7ee7760ff83753392bdaf3e85
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-07 16:50:31 +0000

    Episode 5 complet

---
+++ who_dunnit_ep5.js
@@ -0,0 +1,22 @@
+// Episode 5
+
+const scenario = {
+    murderer: 'Miss Scarlet',
+    room: 'Kitchen',
+    weapon: 'Candle Stick'
+  };
+  
+  const changeWeapon = function(newWeapon) {
+    scenario.weapon = newWeapon;
+  }
+  
+  const declareWeapon = function() {
+    return `The weapon is the ${scenario.weapon}.`;
+  }
+  
+  changeWeapon('Revolver');
+  const verdict = declareWeapon();
+  console.log(verdict);
+
+// Predictions:
+// The weapon is the Revolver.
