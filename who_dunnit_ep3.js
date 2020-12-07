b33c68ea1a7dea63201fa93b48485ed50faad3ad
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-07 16:45:08 +0000

    Episode 3 complet

---
+++ who_dunnit_ep3.js
@@ -0,0 +1,18 @@
+//Episode 3
+
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+
+// Predictions: 
+// firstVerdict = Mrs Peacock (from declareMurderer, anonymous function)
+// secondVerdict = Professor Plum (from murderer variable at top of program)
